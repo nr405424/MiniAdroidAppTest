47ae78668e55486627d577fb8dfb9e1091f69f91
Roppii <ng.roche7@gmail.com>
2020-06-13 18:51:40 +0200

    Toast + Vibrate + TextMeshPro

---
+++ AndroidAppTest/Assets/Scripts/ButtonManagement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManagement : MonoBehaviour
+{
+    AndroidJavaClass ajc;
+
+    void Start() {
+        ajc = new AndroidJavaClass("com.example.myplugin.ButtonHandler");
+        SendToAndroid("Hello World");
+    }
+
+    public void VibratePhone() {
+        ajc.CallStatic("VibratePhone");
+    }
+
+    public void ShowToastMessage(string message) {
+        ajc.CallStatic("ShowToast", message);
+    }
+
+    private void SendToAndroid(string message) {
+        ajc.CallStatic("ShowToast", message);
+    }
+
+    private void ReceiveFromAndroid(string message) {
+        Debug.Log("Received message from toast plugin: " + message);
+    }
+}
